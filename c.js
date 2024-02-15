acd0a9f4ae35201edf6468ee1df5f28b77dc6248
Saad16204 <156604331+Saad16204@users.noreply.github.com>
2024-02-15 12:38:24 +0500

    Add files via upload

---
+++ c.js
@@ -0,0 +1,9 @@
+const  getcolor =() =>{
+    const randomcolor = Math.floor(Math.random ()*16777215);
+    const randomcode = "#"+ randomcolor.toString(16);
+    document.body.style.backgroundColor =randomcode;
+    document.getElementById('color-code').innerText = randomcode;
+ 
+}
+document.getElementById("btn").addEventListener("click",getcolor)
+getcolor();
